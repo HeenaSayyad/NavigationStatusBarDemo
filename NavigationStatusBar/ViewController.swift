e0364a6587faf26f2e2802912ca055ea0f3f5b09
HeenaSayyad <heenapatel3090@gmail.com>
2018-09-14 16:10:16 +0530

    Its a simple NavigationController. which is been drag and drop and get connected to view controller. There 2 view controller get connected.On click of first view controller press button second second page get open with back button.

---
+++ NavigationStatusBar/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NavigationStatusBar
+//
+//  Created by ramjan sayyad on 07/08/18.
+//  Copyright © 2018 ramjan sayyad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+    @IBAction func red(_ sender: UIBarButtonItem) {
+    }
+}
+
